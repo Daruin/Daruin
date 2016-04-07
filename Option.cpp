0a6e79c287977935432602260866c0248a9eae8b
sh4869 <nobuk4869@gmail.com>
2016-04-07 19:04:50 +0900

    Create functions for json in Option Class

--- Option.cpp
+++ Option.cpp
@@ -1,6 +1,57 @@
 #include "Option.h"
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <QUrl>
+#include <QFile>
+#include <QByteArray>
 
 Option::Option()
 {
+    if(!loadSetting()){
+        arduinoDirectoryURL = new QUrl("");
+    }
+}
 
+void Option::read(const QJsonObject &json)
+{
+    arduinoDirectoryURL = new QUrl(json["arduinoDirectory"].toString());
+}
+
+void Option::write(QJsonObject &json) const
+{
+    json["arduinoDirectory"] = arduinoDirectoryURL->toString();
+}
+
+void Option::save(QUrl url)
+{
+    arduinoDirectoryURL = url;
+    saveSetting();
+}
+
+bool Option::loadSetting(void)
+{
+    QFile saveFile("save.json");
+    if(!saveFile.open(QIODevice::ReadOnly)){
+        return false;
+    }
+    QByteArray saveData = saveFile.readAll();
+
+    QJsonDocument loadDoc(QJsonDocument::fromJson(saveData));
+
+    read(loadDoc);
+    return true;
+}
+
+bool Option::saveSetting(void)
+{
+    QFile saveFile("save.json");
+    if(!saveFile.open(QIODevice::WriteOnly)){
+        return false;
+    }
+
+    QJsonObject optionObejct;
+    write(optionObject);
+    QJsonDocument saveDoc(optionObejct);
+    saveFile.write(saveDoc.toJson());
+    return true;
 }
--- Option.h
+++ Option.h
@@ -1,11 +1,22 @@
 #ifndef OPTION_H
 #define OPTION_H
 
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <QUrl>
 
 class Option
 {
 public:
     Option();
+    void read(const QJsonObject &json);
+    void write(QJsonObject &json) const;
+    void save(QUrl url);
+
+private:
+    QUrl *arduinoDirectoryURL;
+    bool loadSetting();
+    bool saveSetting();
 };
 
 #endif // OPTION_H
