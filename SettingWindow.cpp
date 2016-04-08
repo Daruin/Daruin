e69dc2999ab84eb23f229faf0be65452a9870631
sh4869 <nobuk4869@gmail.com>
2016-04-08 23:57:17 +0900

    Create Simple strcture of SettingWindow Class

--- SettingWindow.cpp
+++ SettingWindow.cpp
@@ -1,6 +1,18 @@
 #include "SettingWindow.h"
+#include <QWidget>
+#include <QHBoxLayout>
+#include <QPushButton>
 
 SettingWindow::SettingWindow()
 {
+    ADSettingButton = new QPushButton("Setting Arduino Dialog");
+    mainLayout = new QHBoxLayout;
+    mainLayout->addWidget(ADSettingButton);
+    setLayout(mainLayout);
+    connect(ADSettingButton,SIGNAL(clicked()),this,SLOT(setArduinoDirectory()));
+
+}
+
+void SettingWindow::setArduinoDirectory(){
 
 }
--- SettingWindow.h
+++ SettingWindow.h
@@ -3,10 +3,24 @@
 
 #include <QWidget>
 
+class QPushButton;
+class QFileDialog;
+class QHBoxLayout;
+class QVBoxLayout;
+
 class SettingWindow : public QWidget
 {
+    Q_OBJECT
 public:
     SettingWindow();
+
+public slots:
+    void setArduinoDirectory();
+private:
+    QPushButton* ADSettingButton;
+    QFileDialog* ADDialog;
+    QHBoxLayout* mainLayout;
+
 };
 
 #endif // SETTINGWINDOW_H
